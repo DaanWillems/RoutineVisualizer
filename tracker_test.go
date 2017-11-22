3f200613648693d0a62e2ae027605cab500e5656
daanwillems <dwillems2@avans.nl>
2017-11-22 16:54:47 +0100

    Add neat waitgroups

--- tracker_test.go
+++ tracker_test.go
@@ -1,6 +1,7 @@
 package RoutineVisualizer
 
 import (
+	"sync"
 	"testing"
 	"time"
 )
@@ -9,21 +10,27 @@ func TestTracker(t *testing.T) {
 	tracker1 := NewTracker("Worker 1")
 	tracker2 := NewTracker("Worker 2")
 
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+
 	go func(t Tracker) {
 		time.Sleep(1 * time.Second)
-		WriteEvent("first", t)
+		WriteEvent("first event", t)
 		time.Sleep(1 * time.Second)
-		WriteEvent("second", t)
+		WriteEvent("second event", t)
+		wg.Done()
 	}(tracker1)
 
 	go func(t Tracker) {
 		time.Sleep(1 * time.Second)
-		WriteEvent("first", t)
+		WriteEvent("first event", t)
 		time.Sleep(1 * time.Second)
-		WriteEvent("second", t)
+		WriteEvent("second event", t)
+		wg.Done()
 	}(tracker2)
 
-	time.Sleep(4 * time.Second)
+	wg.Wait()
 
 	EncodeTrackers()
 }
